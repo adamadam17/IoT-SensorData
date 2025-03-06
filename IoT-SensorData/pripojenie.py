3cc68175ace06d9c5b20cd9eba52bf7b3983fef6
adam <95377516+adamadam17@users.noreply.github.com>
2025-03-06 11:32:37 +0100

    Update pripojenie.py

--- IoT-SensorData/pripojenie.py
+++ IoT-SensorData/pripojenie.py
@@ -2,20 +2,20 @@ import paho.mqtt.client as mqtt
 import mysql.connector
 import json
 
-# Konfigurácia MySQL databázy
+
 db = mysql.connector.connect(
     host="localhost",
-    user="root",          # Nahraďte používateľským menom pre MySQL
-    password="databaza",  # Nahraďte heslom pre MySQL
+    user="root",          
+    password="databaza",  
     database="meteostanica"
 )
 
 cursor = db.cursor()
 
-# Funkcia na spracovanie prijatých dát z MQTT
+
 def on_message(client, userdata, msg):
     try:
-        # Prevod prijatých údajov z JSON
+        
         data = json.loads(msg.payload.decode())
         teplota = data['temperature']
         vlhkost = data['humidity']
@@ -23,7 +23,7 @@ def on_message(client, userdata, msg):
         svietivost = data['light_level']
         zrazky = data['rain_status']
 
-        # SQL príkaz na vloženie údajov do tabuľky
+        
         sql = "INSERT INTO sensor_data (teplota, vlhkost, tlak, svietivost, zrazky) VALUES (%s, %s, %s, %s, %s)"
         values = (teplota, vlhkost, tlak, svietivost, zrazky)
         cursor.execute(sql, values)
@@ -33,14 +33,14 @@ def on_message(client, userdata, msg):
     except Exception as e:
         print("Chyba pri spracovaní správy:", e)
 
-# Pripojenie na MQTT broker
+
 client = mqtt.Client()
 client.on_message = on_message
 
-broker = "192.168.0.243"  # IP adresa vášho MQTT brokeru
+broker = "192.168.***.***"  
 client.connect(broker, 1883, 60)
 
-# Prihlásenie na odber témy
+
 client.subscribe("sensor/data")
 
 print("Pripojený na MQTT broker, čakám na dáta...")
